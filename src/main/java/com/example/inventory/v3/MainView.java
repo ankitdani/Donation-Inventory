77c7d384ba33d757c19fb0104a730c3c42fa4793
Ankit Dani <ankitdani1997@gmail.com>
2024-02-08 19:41:25 -0800

    Added Donation Quantity

--- src/main/java/com/example/inventory/v3/MainView.java
+++ src/main/java/com/example/inventory/v3/MainView.java
@@ -1,6 +1,5 @@
 package com.example.inventory.v3;
 
-
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
@@ -20,10 +19,10 @@ public class MainView extends VerticalLayout {
 
     TextField donorNameField = new TextField("Enter donor name:");
     DatePicker donationDateField = new DatePicker("Enter donation date:");
-
+    TextField quantityField = new TextField("Enter quantity:");
     DonationService donationService;
-
     Grid<Donor> grid = new Grid<>(Donor.class);
+
     public MainView(DonationService donationService) {
         this.donationService = donationService;
         addDonorForm();
@@ -33,21 +32,28 @@ public class MainView extends VerticalLayout {
     public void addDonorForm(){
         donorNameField.setPlaceholder("Firstname Lastname");
         donationDateField.setPlaceholder("Pacific Standard Time");
+        quantityField.setPlaceholder("");
         Button saveButton = new Button("Save", event -> saveDonor());
         saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         add(donorNameField, saveButton);
         add(donationDateField, saveButton);
+        add(quantityField, saveButton);
     }
 
     public void saveDonor() {
         Donor donor = new Donor();
         donor.setDonorName(donorNameField.getValue());
         donationService.saveDonor(donor);
+
         // Convert LocalDate to Date
         LocalDate localDate = donationDateField.getValue();
         Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
         Date date = Date.from(instant);
+
         donor.setDonationDate(date);
+        donationService.saveDonor(donor);
+
+        donor.setQuantity(quantityField.getValue());
         donationService.saveDonor(donor);
     }
 
