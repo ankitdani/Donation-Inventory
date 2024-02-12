b18bee3e6a4e660a08d5c919bc839e0decf2e9dc
Ankit Dani <ankitdani1997@gmail.com>
2024-02-11 16:13:40 -0800

    Donor entity class cleanup

--- src/main/java/com/example/inventory/v3/Donor.java
+++ src/main/java/com/example/inventory/v3/Donor.java
@@ -1,11 +1,5 @@
 package com.example.inventory.v3;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import java.util.Date;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
