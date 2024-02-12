745fd412b394d5ceca31750edc7acec5afcf3288
Ankit Dani <ankitdani1997@gmail.com>
2024-02-11 16:12:44 -0800

    Donation Service class cleanup

--- src/main/java/com/example/inventory/v3/DonationService.java
+++ src/main/java/com/example/inventory/v3/DonationService.java
@@ -6,22 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
-//public class DonationService {
-//    @Autowired
-//    DonorRepository donorRepository;
-//    public List<Donor> getAllDonors(){
-//        return donorRepository.findAll();
-//    }
-//    public Donor saveDonor(Donor donor){
-//        return donorRepository.save(donor);
-//    }
-//}
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -35,21 +19,15 @@ public class DonationService {
 
     public List<Donor> getAllDonors(){
         LOGGER.info("Entering getAllDonors() method");
-//        System.out.println("Entering getAllDonors() method");
         List<Donor> donors = donorRepository.findAll();
         LOGGER.info("Found " + donors.size() + " donors");
         LOGGER.info("Exiting getAllDonors() method");
-//        System.out.println("Found " + donors.size() + " donors");
-//        System.out.println("Exiting getAllDonors() method");
         return donors;
     }
 
     public Donor saveDonor(Donor donor){
         LOGGER.info("Entering saveDonor() method");
-//        System.out.println("Entering saveDonor() method");
         Donor savedDonor = donorRepository.save(donor);
-//        System.out.println("Saved donor with name: " + savedDonor.getDonorName());
-//        System.out.println("Exiting saveDonor() method");
         LOGGER.info("Saved donor with name: {}" + savedDonor.getDonorName());
         LOGGER.info("Exiting saveDonor() method");
         return savedDonor;
