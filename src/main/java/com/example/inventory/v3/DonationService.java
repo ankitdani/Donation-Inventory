10de2b3c2f2e7b0f104a31510e72439e640c1402
Ankit Dani <ankitdani1997@gmail.com>
2024-02-11 16:13:07 -0800

    Donation Service class cleanup

--- src/main/java/com/example/inventory/v3/DonationService.java
+++ src/main/java/com/example/inventory/v3/DonationService.java
@@ -1,6 +1,5 @@
 package com.example.inventory.v3;
 
-import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
